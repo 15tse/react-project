1119e0db05bb8a29183bd7c23a30f708d92867ac
tsegaye abadi <tsegaye.abadi15@gmail.com>
2024-12-02 12:00:49 -0500

    react file added

---
+++ NewApp/newapp/src/App.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+
+import LabTestComponent from './component/LabTestComponent';
+
+
+function App() {
+  return (
+    <div>
+      <LabTestComponent />
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+
